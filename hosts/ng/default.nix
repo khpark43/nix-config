e51230b51b21d7aed33009b678544e559eefab8e
Kyunghyun Park <me@khp.dev>
2025-01-08 17:35:53 +0900

    ng zsh/docker

--- hosts/ng/default.nix
+++ hosts/ng/default.nix
@@ -7,9 +7,23 @@
   wsl.enable = true;
   wsl.defaultUser = "khp";
 
-  networking = {
-    hostName = "ng";
+  networking.hostName = "ng";
+
+  virtualisation.docker.enable = true;
+
+  time.timeZone = "Asia/Seoul";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  users.users.khp = {
+    isNormalUser = true;
+    description = "Kyunghyun Park";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.zsh;
   };
+
+  programs.zsh.enable = true;
+  nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = with pkgs; [
     git
@@ -17,5 +31,9 @@
     wget
     curl
   ];
+
+  environment.variables = {
+    EDITOR = "vim";
+  };
   system.stateVersion = "24.05";
 }
