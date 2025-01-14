3112d538320964bbb5beeae70d97e289b4fe56ba
Kyunghyun Park <me@khp.dev>
2025-01-14 15:04:03 +0900

    nixpkgs.follows unstable

--- flake.nix
+++ flake.nix
@@ -4,14 +4,22 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
-    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
-    nix-darwin.url = "github:LnL7/nix-darwin";
-
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixvim.url = "github:khpark43/nixvim-config";
     nvf.url = "github:khpark43/nvf-config";
     firefox-addons = {
