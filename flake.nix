e64ba025bf8cec7db4d4248d553f56435345ef88
Kyunghyun Park <me@khp.dev>
2025-01-18 02:07:11 +0900

    wip

--- flake.nix
+++ flake.nix
@@ -30,84 +30,82 @@
     };
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      # nixpkgs-stable,
-      home-manager,
-      nixos-wsl,
-      nix-darwin,
-      nvf,
-      ...
-    }@inputs:
-    let
-      outputs = self;
-      configModule = import ./nvf-conf.nix false;
-      forAllSystems = nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-    in
-    {
-      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);
-      packages = forAllSystems (system: {
-        my-neovim =
-          (nvf.lib.neovimConfiguration {
-            pkgs = nixpkgs.legacyPackages.${system};
-            modules = [ configModule ];
-          }).neovim;
-      });
-      nixosConfigurations = {
-        up = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [ ./hosts/up ];
-          specialArgs = { inherit inputs outputs; };
-        };
-        down = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            nixos-wsl.nixosModules.default
-            ./hosts/down
-          ];
-          specialArgs = { inherit inputs outputs; };
-        };
+  outputs = {
+    self,
+    nixpkgs,
+    # nixpkgs-stable,
+    home-manager,
+    nixos-wsl,
+    nix-darwin,
+    nvf,
+    ...
+  } @ inputs: let
+    outputs = self;
+    configModule = import ./nvf-conf.nix true;
+    forAllSystems = nixpkgs.lib.genAttrs [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+  in {
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);
+    packages = forAllSystems (system: {
+      my-neovim =
+        (nvf.lib.neovimConfiguration {
+          pkgs = nixpkgs.legacyPackages.${system};
+          modules = [configModule];
+        })
+        .neovim;
+    });
+    nixosConfigurations = {
+      up = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [./hosts/up];
+        specialArgs = {inherit inputs outputs;};
       };
-      darwinConfigurations = {
-        muon = nix-darwin.lib.darwinSystem {
-          modules = [ ./hosts/muon ];
-          specialArgs = { inherit inputs outputs; };
-        };
+      down = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          nixos-wsl.nixosModules.default
+          ./hosts/down
+        ];
+        specialArgs = {inherit inputs outputs;};
       };
-      homeConfigurations = {
-        "khp@up" = home-manager.lib.homeManagerConfiguration {
-          modules = [ ./home/khp/up.nix ];
-          pkgs = import nixpkgs {
-            system = "x86_64-linux";
-            config.allowUnfree = true;
-          };
-          extraSpecialArgs = { inherit inputs outputs; };
+    };
+    darwinConfigurations = {
+      muon = nix-darwin.lib.darwinSystem {
+        modules = [./hosts/muon];
+        specialArgs = {inherit inputs outputs;};
+      };
+    };
+    homeConfigurations = {
+      "khp@up" = home-manager.lib.homeManagerConfiguration {
+        modules = [./home/khp/up.nix];
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
         };
-        "khp@down" = home-manager.lib.homeManagerConfiguration {
-          modules = [ ./home/khp/down.nix ];
-          pkgs = import nixpkgs {
-            system = "x86_64-linux";
-            config.allowUnfree = true;
-          };
-          extraSpecialArgs = { inherit inputs outputs; };
+        extraSpecialArgs = {inherit inputs outputs;};
+      };
+      "khp@down" = home-manager.lib.homeManagerConfiguration {
+        modules = [./home/khp/down.nix];
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
         };
-        "khp@muon" = home-manager.lib.homeManagerConfiguration {
-          modules = [ ./home/khp/muon.nix ];
-          pkgs = import nixpkgs {
-            system = "aarch64-darwin";
-            config.allowUnfree = true;
-          };
-          extraSpecialArgs = {
-            inherit inputs outputs;
-          };
+        extraSpecialArgs = {inherit inputs outputs;};
+      };
+      "khp@muon" = home-manager.lib.homeManagerConfiguration {
+        modules = [./home/khp/muon.nix];
+        pkgs = import nixpkgs {
+          system = "aarch64-darwin";
+          config.allowUnfree = true;
+        };
+        extraSpecialArgs = {
+          inherit inputs outputs;
         };
       };
     };
+  };
 }
