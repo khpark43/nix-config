d4e9251a9c7e220644dcb6d4dfad484d957d6f1d
Kyunghyun Park <me@khp.dev>
2025-01-31 17:57:48 +0900

    git config

--- home/khp/cli/git.nix
+++ home/khp/cli/git.nix
@@ -3,10 +3,16 @@
   programs.git = {
     enable = true;
     userName = "Kyunghyun Park";
-    userEmail = "me@khp.dev";
+    userEmail = lib.mkDefault "me@khp.dev";
     extraConfig = {
       init.defaultBranch = "main";
+      user.signing.key = "3586A37F5FBE2C33F3563DA82ABFA32C5F0545FF";
       commit.gpgSign = lib.mkDefault true;
+      push.autoSetupRemote = true;
     };
+    ignores = [
+      ".direnv"
+      "result"
+    ];
   };
 }
