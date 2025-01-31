c35e00b98e3aaaf8ed528988e2d4b1387dd099c5
Kyunghyun Park <me@khp.dev>
2025-01-31 17:58:00 +0900

    zsh config

--- home/khp/cli/zsh.nix
+++ home/khp/cli/zsh.nix
@@ -1,6 +1,7 @@
 _: {
   programs.zsh = {
     enable = true;
+    defaultKeymap = "viins";
     oh-my-zsh = {
       enable = true;
       plugins = [ "git" ];
@@ -15,5 +16,16 @@ _: {
       hm = "home-manager --flake .";
       hms = "home-manager --flake . switch";
     };
+    initExtra = ''
+      autoload edit-command-line; zle -N edit-command-line
+      bindkey '^v' edit-command-line
+
+      bindkey "^?" backward-delete-char
+
+      bindkey -M menuselect 'h' vi-backward-char
+      bindkey -M menuselect 'j' vi-down-line-or-history
+      bindkey -M menuselect 'k' vi-up-line-or-history
+      bindkey -M menuselect 'l' vi-forward-char
+    '';
   };
 }
